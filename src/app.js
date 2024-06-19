4c511b307d13a6b48cb7ec0018f4ab176f2a9034
Dmitry Boytsov <135224601+Boytsov-Dmitry@users.noreply.github.com>
2024-06-19 19:09:35 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,8 @@ export default class Team {
   };
 
   *[Symbol.iterator]() {
-    let index = 0;
-    while (index < this.members.length) {
-      yield this.members[index];
-      index++;
+    for (let item of this.members) {
+        yield item;
     };
   };
   
